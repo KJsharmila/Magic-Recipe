fb696ee59272d439ac5c04318b72fe94ce15976b
KJsharmila <sjayaramu@qwinixtech.com>
2017-06-24 14:50:32 +0530

    database and functionality

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,4 +1,13 @@
 class RecipeController < ApplicationController
   def index
   end
+  def search
+        @recipe = @recipe.ingredient
+        @recipes = Plan::FoodSelection.new(@ingredient).search
+        if @recipes.present?
+          render :json => {:success => true, :response => @recipes}, :status => 201
+        else
+          render :json => {:success => false, :errors => ["No recipes for this service providers"]}, :status => 422
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'recipe/index'
   root 'recipe#index'
-
+resources :recipes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
